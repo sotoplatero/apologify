5c42bb0ef2771e6ead14a1703282f9b2ec4defa5
Damian Soto <soto.platero@gmail.com>
2024-10-19 12:08:28 -0400

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import vercel from '@astrojs/vercel/serverless';
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
-  site: "https://writeapologyletter.com",
+  site: "https://bestapologyletters.com",
   integrations: [tailwind(), sitemap(), svelte(), preact()],
   adapter: vercel(),
 });
