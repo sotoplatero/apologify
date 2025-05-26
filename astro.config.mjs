1024ef2066ad16aac59ff6723042a96522188f46
unknown <soto.platero@gmail.com>
2025-05-26 10:50:23 -0400

    update hero and cta

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,12 +8,11 @@ import svelte from '@astrojs/svelte';
 import preact from '@astrojs/preact';
 
 import vercel from '@astrojs/vercel';
-import db from '@astrojs/db';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   site: "https://apologify.com",
-  integrations: [tailwind(), sitemap(), svelte(), preact(), db()],
+  integrations: [tailwind(), sitemap(), svelte(), preact()],
   adapter: vercel({ 
     isr: {
       exclude: [ "/generator", "/generator/wizard" ]
